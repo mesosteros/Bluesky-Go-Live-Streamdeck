2d378a87681fe4055350850f70933e589d70cb87
Carlos Santos <1943233+mesosteros@users.noreply.github.com>
2026-07-01 01:09:40 +0100

    Remove duplicated end live status action (I was silly due to tiredness)

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,7 +1,6 @@
 import streamDeck from '@elgato/streamdeck';
 
 import { BlueskyPostAction } from './actions/bluesky-post';
-import { BlueSkyEndAction } from './actions/bluesky-end';
 import { BlueskyPostOnlyAction } from './actions/bluesky-post-only';
 import { BlueskySetStatusAction } from './actions/bluesky-set-status';
 import { BluskyClearStatusAction } from './actions/bluesky-clear-status';
@@ -11,7 +10,6 @@ streamDeck.logger.setLevel('trace');
 
 // Register the actions.
 streamDeck.actions.registerAction(new BlueskyPostAction());
-streamDeck.actions.registerAction(new BlueSkyEndAction());
 streamDeck.actions.registerAction(new BlueskyPostOnlyAction());
 streamDeck.actions.registerAction(new BlueskySetStatusAction());
 streamDeck.actions.registerAction(new BluskyClearStatusAction());
